573d373d4c5342ee3dca3fb74b455e4c44e47022
Anders Hafreager <andershaf@gmail.com>
2021-03-26 13:20:05 +0100

    Updates

---
+++ precache-manifest.08937595a12a4c210b1b88f9a025da0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0539868fada53da11a38ffe184a373b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c18e5c7b45a677882cbe",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "f05dde81ad6d62280b79",
+    "url": "./static/js/2.0016d0f4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0016d0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c18e5c7b45a677882cbe",
+    "url": "./static/js/main.dad8957a.chunk.js"
+  },
+  {
+    "revision": "8933b7cafb8afed186ac",
+    "url": "./static/js/runtime-main.70cca2e1.js"
+  }
+]);
