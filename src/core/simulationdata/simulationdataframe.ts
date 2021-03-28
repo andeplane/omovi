430f025c11622417eae5c6249e9b9bdc339923b6
Anders Hafreager <andershaf@gmail.com>
2021-03-28 13:45:13 +0200

    Storing unique particles types on dataframe

--- src/core/simulationdata/simulationdataframe.ts
+++ src/core/simulationdata/simulationdataframe.ts
@@ -2,9 +2,19 @@ import Particles from 'core/geometries/particles/particles'
 import Bonds from 'core/geometries/bonds/bonds'
 
 export default class SimulationDataFrame {
+  particleTypes: string[]
   particles: Particles
   bonds?: Bonds
+
   constructor(particles: Particles) {
     this.particles = particles
+
+    const particleTypeMap: { [key: string]: boolean } = {}
+    particles.types.forEach((particleType: string) => {
+      particleTypeMap[particleType] = true
+    })
+
+    // @ts-ignore
+    this.particleTypes = Object.keys(particleTypeMap)
   }
 }
