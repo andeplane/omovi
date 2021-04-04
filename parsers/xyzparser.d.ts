c154d012ddfc255066270ece699c357e9d5d1b1c
andeplane <andeplane@users.noreply.github.com>
2021-04-04 15:06:38 +0000

    deploy: e97126ab97c512c026d38dd9c5fa2446d2db5ce6

--- parsers/xyzparser.d.ts
+++
@@ -1,3 +0,0 @@
-import SimulationData from 'core/simulationdata/simulationdata';
-declare const parseXyz: (data: string) => SimulationData;
-export default parseXyz;
---
+++ precache-manifest.296caa61c7e9729c792137090bb1cf70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6aafd5e1989158a2f2676d19e9b768c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2fed8978ff18e97914da",
+    "url": "./static/css/2.844aa0ee.chunk.css"
+  },
+  {
+    "revision": "df3eb81447b9b677908c",
+    "url": "./static/css/main.c2a58695.chunk.css"
+  },
+  {
+    "revision": "2fed8978ff18e97914da",
+    "url": "./static/js/2.39c62b2d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.39c62b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df3eb81447b9b677908c",
+    "url": "./static/js/main.29e3edde.chunk.js"
+  },
+  {
+    "revision": "8933b7cafb8afed186ac",
+    "url": "./static/js/runtime-main.70cca2e1.js"
+  }
+]);
